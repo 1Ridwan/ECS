1a41cca3969fc8c0ce3c61306d74dd8e80da69b3
1Ridwan <97636059+1Ridwan@users.noreply.github.com>
2025-09-12 00:37:18 +0100

    configure alb, ecs

--- terraform/modules/alb/main.tf
+++ terraform/modules/alb/main.tf
@@ -19,3 +19,33 @@ resource "aws_lb" "main" {
     name = "main"
   }
 }
+
+
+# this should point to the ECS tasks
+
+resource "aws_lb_target_group" "ecs" {
+  name     = "alb-target-group"
+  port     = 80
+  protocol = "TCP"
+  vpc_id   = aws_vpc.main.id
+
+  health_check {
+    healthy_threshold = "2"
+    internal = "30"
+    path - "/"
+    port = "80"
+    protocol = "HTTP"
+
+  }
+}
+
+resource "aws_lb_listener" "front_end" {
+  load_balancer_arn = aws_lb.main.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.ecs.arn
+  }
+}
