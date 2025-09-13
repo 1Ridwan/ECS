10fa120b9a60d4871997960f3c59f7f2b6541b32
1Ridwan <97636059+1Ridwan@users.noreply.github.com>
2025-09-13 23:09:56 +0100

    create list of subnet ids for public subnets

--- terraform/modules/vpc/outputs.tf
+++ terraform/modules/vpc/outputs.tf
@@ -4,6 +4,10 @@ output "vpc_id" {
 
 output "public_subnet_ids" {
 
-    value = [for s in aws_subnet.set : s =>  if s.map_public_ip_on_launch = true]
-
+    value = [for k, v in aws_subnet.set : aws_subnet.set[k].id if aws_subnet.set[k].map_public_ip_on_launch == true]
 }
+
+# example below:
+
+#    value = [for a in var.objects : upper(a) if substr(a,0,5) != "lower"]
+# subnet_ids = [for k, v in aws_subnet.private : aws_subnet.private[k].id]
