93f52e67c9dc829afe6f573eb2ebbfcfdc38d538
1Ridwan <97636059+1Ridwan@users.noreply.github.com>
2025-09-20 02:32:05 +0100

    remove dynamodb comment

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -3,7 +3,5 @@ terraform {
     bucket         = "terraform-state-ridwan-ecs"
     key            = "terraform.tfstate"
     region         = "eu-west-2"
-   # dynamodb_table = "terraform-locks-ecs-project"
-    # encrypt        = true
   }
 }
