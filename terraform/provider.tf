be518232512ac07929a194870a690ff1b677b7bd
1Ridwan <97636059+1Ridwan@users.noreply.github.com>
2025-09-20 17:07:16 +0100

    run terraform apply pipeline

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,10 +1,13 @@
 terraform {
-  required_version = ">= 1.6.0"
+  required_version = ">= 1.12.0"
   required_providers {
-    aws = { source = "hashicorp/aws", version = ">= 6.9.0, < 7.0.0" }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.12.0"
+    }
   }
 }
 
 provider "aws" {
-  region  = "eu-west-2"
+  region = var.region
 }
