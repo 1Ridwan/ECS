0a2d37625bab5ef76e97021d32cae0f523fce422
1Ridwan <97636059+1Ridwan@users.noreply.github.com>
2025-09-21 21:16:53 +0100

    update nginx config

--- terraform/modules/route53/main.tf
+++ terraform/modules/route53/main.tf
@@ -2,7 +2,6 @@ data "aws_route53_zone" "primary" {
   name         = "ridwanprojects.com."
 }
 
-
 resource "aws_route53_record" "www" {
   zone_id = data.aws_route53_zone.primary.zone_id
   name    = "ridwanprojects.com"
